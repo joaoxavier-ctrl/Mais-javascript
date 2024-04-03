cd3c15c035bdfa02ac74b3e4392fb9308ddf372a
JoaoXavierWin <joao.xavier21@gmail.com>
2024-04-03 19:44:34 -0300

    factory-functions

---
+++ factory-function/script-aula.js
@@ -0,0 +1,35 @@
+/*function createButton(text){
+    function element(){
+        const buttonElement = document.createElement('button');
+        buttonElement.innerText = text;
+        return buttonElement;
+    }
+    return Object.freeze({
+        text,
+        element
+    });
+}
+
+const btnComprar = createButton('Comprar');
+const btnVender = createButton('Vender');
+
+btnComprar.text = 'Novo texto'
+
+console.log(btnComprar, btnVender);
+
+function Pessoa(nome){
+    if(!(this instanceof Pessoa)){
+        return new Pessoa(nome)
+    }
+    console.log(this);
+    this.nome = nome;
+}
+
+Pessoa.prototype.andar = function(){
+    return `${this.nome} andou`;
+}
+
+const designer = new Pessoa('João');
+
+console.log(designer);
+*/
